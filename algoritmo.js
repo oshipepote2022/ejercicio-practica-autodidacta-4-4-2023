fa3b077990bdcafef704d7fd79675fdfc7dc9c8c
Gustavo <gustavo_r69@yahoo.com.ar>
2023-04-09 22:36:41 -0300

    entrega practica ejercicio autodidacta 4-4-2023

---
+++ algoritmo.js
@@ -0,0 +1,6 @@
+let resultado = prompt(" ingresa el nombre del proyecto");
+let VerificacionGitHubRepo = confirm("Verificaron la subida del proyecto en github?")
+
+//devuelvo un booleano pero no lo guardo//
+alert(resultado);
+alert("Verificacion del repositorio: "+ VerificacionGitHubRepo);
